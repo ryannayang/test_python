14c429406996e9a0bcf1e4a37463fdf6b19c3eb7
Brendan <brendan.development@pm.me>
2023-02-21 20:20:21 -0800

    added datetime as a new function input

--- github_project.py
+++ github_project.py
@@ -5,9 +5,18 @@ today = today.strftime("%d/%m/%Y")
 
 
 def greeting(name):
-    print(f"Hello {name}, welcome to our project! Today is {today}")
+    print(f"Hello {name}, Welcome to our project!")
 
 
-name = input("What is your name?")
+def datetime_request():
+    date_input = input("Would you like to know what the date is? (y/n): ")
+    if date_input.lower() == "y" or date_input.lower() == "yes":
+        print(f"Okay, Today is {today}")
+    else:
+        print("Okay, Have a great day :)")
+
+
+name = input("What is your name? ")
 
 greeting(name)
+datetime_request()
