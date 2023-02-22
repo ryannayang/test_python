91fab5f8fc4c9fb0c37bceb68237f0d608f3df80
Brendan <brendan.development@pm.me>
2023-02-21 19:59:21 -0800

    added datetime in greeting message

--- github_project.py
+++ github_project.py
@@ -1,8 +1,13 @@
+import datetime
+
+today = datetime.datetime.today()
+today = today.strftime("%d/%m/%Y")
+
 
 def greeting(name):
-	print(f"Hello {name}, welcome to our project!")
+    print(f"Hello {name}, welcome to our project! Today is {today}")
+
 
 name = input("What is your name?")
 
 greeting(name)
-
